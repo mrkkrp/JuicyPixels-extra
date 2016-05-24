e21d7387e994aafe31592baebc1017cc7e5f6070
mrkkrp <markkarpov@opmbx.org>
2016-05-24 22:33:47 +0700

    Add test for the crop operation

--- tests/Codec/Picture/ExtraSpec.hs
+++ tests/Codec/Picture/ExtraSpec.hs
@@ -3,11 +3,34 @@ module Codec.Picture.ExtraSpec
   , spec )
 where
 
--- import Codec.Picture.Extra
+import Codec.Picture
+import Codec.Picture.Extra
+import Control.Monad
+import Data.Function (on)
 import Test.Hspec
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = return () -- TODO write tests
+spec =
+  describe "crop" cropSpec
+
+cropSpec :: Spec
+cropSpec =
+  context "when we pass arguments within image size" $
+    it "produces correct image" $ do
+      (Right (ImageRGB8 original)) <- readImage "data-examples/lenna.png"
+      (Right (ImageRGB8 cropped)) <- readImage "data-examples/lenna-cropped.png"
+      let result = crop 211 210 178 191 original
+      result `blindlySatisfy` sameImage cropped
+
+blindlySatisfy :: a -> (a -> Bool) -> Expectation
+v `blindlySatisfy` p =
+  unless (p v) (expectationFailure "predicate failed")
+
+sameImage :: Image PixelRGB8 -> Image PixelRGB8 -> Bool
+sameImage a b =
+  ((==) `on` imageWidth)  a b &&
+  ((==) `on` imageHeight) a b &&
+  ((==) `on` imageData)   a b
